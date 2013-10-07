4c1a113d95b8040ef13570d64468d05e8b5777ca
Tyler Funk <tcfunk@eiu.edu>
2013-10-07 13:32:32 -0500

    Exercise 34 complete

---
+++ ex34.py
@@ -0,0 +1,11 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+assert(animals[1] == 'python')
+assert(animals[2] == 'peacock')
+assert(animals[0] == 'bear')
+assert(animals[3] == 'kangaroo')
+assert(animals[4] == 'whale')
+assert(animals[2] == 'peacock')
+assert(animals[5] == 'platypus')
+assert(animals[4] == 'whale')
+
